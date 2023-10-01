da3ba95a76ba1a4c2b3154c5740f86e5266c18de
Kerimov Ahmed <kerimov.dev@yandex.ru>
2023-10-01 23:25:39 +0300

    Add lab02

---
+++ lab02/types/common.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <set>
+#include <unordered_set>
+#include <vector>
+
+
+template <typename T>
+using Set = std::set<T>;
+
+template <typename T>
+using USet = std::unordered_set<T>;
+
+template <typename T>
+using Vector = std::vector<T>;
