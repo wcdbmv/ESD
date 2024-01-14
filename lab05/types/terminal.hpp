bba02a3f1a42967d3f9a010c43a8d258e48d6bf2
Ahmed Kerimov <kerimov.dev@yandex.ru>
2024-01-14 20:22:01 +0300

    Add lab05

---
+++ lab05/types/terminal.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "types/name.hpp"
+
+#include <iosfwd>
+
+struct Constant;
+struct Variable;
+
+struct Terminal {
+	Name name;
+
+	explicit Terminal(std::string_view name)
+		: name(name.begin(), name.end()) {}
+
+	[[nodiscard]] virtual bool IsConstant() const noexcept = 0;
+	[[nodiscard]] bool IsVariable() const noexcept { return !IsConstant(); }
+
+	[[nodiscard]] const Constant& AsConstant() const noexcept;
+	[[nodiscard]] const Variable& AsVariable() const noexcept;
+};
+
+using TerminalPtr = Terminal*;
+using TerminalCPtr = const Terminal*;
+using TerminalCRef = const Terminal&;
+
+std::ostream& operator<<(std::ostream& os, TerminalCRef terminal);
+std::ostream& operator<<(std::ostream& os, TerminalCPtr terminal);
